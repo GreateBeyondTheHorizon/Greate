7ba72253c9e1258bb03d16af30f052939807491c
Electrolyte220 <cjm03c@gmail.com>
2023-08-18 22:23:37 -0400

    Add tiered alloys

--- src/main/java/electrolyte/greate/registry/ModItems.java
+++ src/main/java/electrolyte/greate/registry/ModItems.java
@@ -1,6 +1,8 @@
 package electrolyte.greate.registry;
 
+import com.tterrag.registrate.util.entry.ItemEntry;
 import electrolyte.greate.Greate;
+import net.minecraft.world.item.Item;
 
 import static electrolyte.greate.Greate.REGISTRATE;
 
@@ -9,36 +11,52 @@ public class ModItems {
     static {
         REGISTRATE.creativeModeTab(() -> Greate.GREATE_TAB);
     }
-/*
 
     public static final ItemEntry<Item> STEEL_ALLOY = REGISTRATE
             .item("steel_alloy", Item::new)
+            .model((c, p) -> p.withExistingParent(c.getName(), "item/generated")
+                    .texture("layer0", p.modLoc("item/" + c.getName().substring(0, c.getName().length() - 6) + "/alloy")))
             .register();
     public static final ItemEntry<Item> ALUMINIUM_ALLOY = REGISTRATE
             .item("aluminium_alloy", Item::new)
+            .model((c, p) -> p.withExistingParent(c.getName(), "item/generated")
+                    .texture("layer0", p.modLoc("item/" + c.getName().substring(0, c.getName().length() - 6) + "/alloy")))
             .register();
     public static final ItemEntry<Item> STAINLESS_STEEL_ALLOY = REGISTRATE
             .item("stainless_steel_alloy", Item::new)
+            .model((c, p) -> p.withExistingParent(c.getName(), "item/generated")
+                    .texture("layer0", p.modLoc("item/" + c.getName().substring(0, c.getName().length() - 6) + "/alloy")))
             .register();
     public static final ItemEntry<Item> TITANIUM_ALLOY = REGISTRATE
             .item("titanium_alloy", Item::new)
+            .model((c, p) -> p.withExistingParent(c.getName(), "item/generated")
+                    .texture("layer0", p.modLoc("item/" + c.getName().substring(0, c.getName().length() - 6) + "/alloy")))
             .register();
     public static final ItemEntry<Item> TUNGSTENSTEEL_ALLOY = REGISTRATE
             .item("tungstensteel_alloy", Item::new)
+            .model((c, p) -> p.withExistingParent(c.getName(), "item/generated")
+                    .texture("layer0", p.modLoc("item/" + c.getName().substring(0, c.getName().length() - 6) + "/alloy")))
             .register();
     public static final ItemEntry<Item> PALLADIUM_ALLOY = REGISTRATE
             .item("palladium_alloy", Item::new)
+            .model((c, p) -> p.withExistingParent(c.getName(), "item/generated")
+                    .texture("layer0", p.modLoc("item/" + c.getName().substring(0, c.getName().length() - 6) + "/alloy")))
             .register();
     public static final ItemEntry<Item> NAQUADAH_ALLOY = REGISTRATE
             .item("naquadah_alloy", Item::new)
+            .model((c, p) -> p.withExistingParent(c.getName(), "item/generated")
+                    .texture("layer0", p.modLoc("item/" + c.getName().substring(0, c.getName().length() - 6) + "/alloy")))
             .register();
     public static final ItemEntry<Item> DARMSTADTIUM_ALLOY = REGISTRATE
             .item("darmstadtium_alloy", Item::new)
+            .model((c, p) -> p.withExistingParent(c.getName(), "item/generated")
+                    .texture("layer0", p.modLoc("item/" + c.getName().substring(0, c.getName().length() - 6) + "/alloy")))
             .register();
     public static final ItemEntry<Item> NEUTRONIUM_ALLOY = REGISTRATE
             .item("neutronium_alloy", Item::new)
+            .model((c, p) -> p.withExistingParent(c.getName(), "item/generated")
+                    .texture("layer0", p.modLoc("item/" + c.getName().substring(0, c.getName().length() - 6) + "/alloy")))
             .register();
-*/
 
     public static void register() {}
 }
