b1ad6ce727bad7d10dbd76b8f441335b020488f7
Electrolyte220 <cjm03c@gmail.com>
2025-07-22 19:54:01 -0400

    Fix create kubejs recipes not being inherited

--- src/main/java/electrolyte/greate/compat/kubejs/KubeJSGreatePlugin.java
+++ src/main/java/electrolyte/greate/compat/kubejs/KubeJSGreatePlugin.java
@@ -1,5 +1,6 @@
 package electrolyte.greate.compat.kubejs;
 
+import com.google.gson.JsonArray;
 import com.gregtechceu.gtceu.api.capability.recipe.EURecipeCapability;
 import com.gregtechceu.gtceu.api.capability.recipe.FluidRecipeCapability;
 import com.gregtechceu.gtceu.api.capability.recipe.ItemRecipeCapability;
@@ -8,7 +9,11 @@ import com.gregtechceu.gtceu.api.recipe.content.Content;
 import com.gregtechceu.gtceu.integration.kjs.recipe.GTRecipeSchema;
 import com.gregtechceu.gtceu.integration.kjs.recipe.GTRecipeSchema.GTRecipeJS;
 import com.gregtechceu.gtceu.integration.kjs.recipe.components.GTRecipeComponents;
+import com.simibubi.create.content.processing.recipe.HeatCondition;
+import com.simibubi.create.content.processing.recipe.ProcessingOutput;
+import com.simibubi.create.foundation.fluid.FluidIngredient;
 import dev.latvian.mods.kubejs.KubeJSPlugin;
+import dev.latvian.mods.kubejs.create.ProcessingRecipeSchema.ProcessingRecipeJS;
 import dev.latvian.mods.kubejs.recipe.RecipeJS;
 import dev.latvian.mods.kubejs.recipe.RecipesEventJS;
 import dev.latvian.mods.kubejs.recipe.schema.RecipeSchema;
@@ -24,9 +29,12 @@ import electrolyte.greate.content.processing.recipe.TieredProcessingRecipeBuilde
 import electrolyte.greate.content.processing.recipe.TieredProcessingRecipeSerializer;
 import electrolyte.greate.foundation.data.recipe.GreateCraftingComponents;
 import electrolyte.greate.registry.ModRecipeTypes;
+import net.minecraft.core.NonNullList;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.item.crafting.Recipe;
 import net.minecraft.world.item.crafting.RecipeManager;
+import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fml.ModList;
 
 import java.util.Arrays;
@@ -66,17 +74,15 @@ public class KubeJSGreatePlugin extends KubeJSPlugin {
     @Override
     public void injectRuntimeRecipes(RecipesEventJS event, RecipeManager manager, Map<ResourceLocation, Recipe<?>> recipesByName) {
         for(RecipeJS r : event.addedRecipes) {
+            if(r.getId().endsWith("_manual_only")) continue;
+            if(r.getId().endsWith("_electric_only")) continue;
             if(r instanceof GTRecipeJS gtRecipeJS) {
-                if(r.getId().endsWith("_manual_only")) continue;
-                if(r.getId().endsWith("_electric_only")) continue;
                 TieredProcessingRecipeFactory<TieredProcessingRecipe<?>> factory = GreateValues.getFactory(gtRecipeJS.getType());
                 if(factory == null) continue;
-                TieredProcessingRecipeBuilder<TieredProcessingRecipe<?>> builder = new TieredProcessingRecipeBuilder<>(factory, Greate.id("integration/" + gtRecipeJS.idWithoutType().toString().replace(":", "/")));
+                TieredProcessingRecipeBuilder<TieredProcessingRecipe<?>> builder = new TieredProcessingRecipeBuilder<>(factory, Greate.id("integration/" + gtRecipeJS.idWithoutType().toString().replace(':', '/')));
                 if(gtRecipeJS.getValue(GTRecipeSchema.DURATION) != null) {
                     builder.duration(gtRecipeJS.getValue(GTRecipeSchema.DURATION).intValue());
-                } else {
-                    builder.averageProcessingDuration();
-                }
+                } else builder.averageProcessingDuration();
                 if(gtRecipeJS.getValue(GTRecipeSchema.ALL_INPUTS) != null) {
                     Map<RecipeCapability<?>, List<Content>> inputs = gtRecipeJS.getValue(GTRecipeSchema.ALL_INPUTS).entrySet().stream()
                             .map(e -> Map.entry(e.getKey(), Arrays.stream(e.getValue())
@@ -127,6 +133,47 @@ public class KubeJSGreatePlugin extends KubeJSPlugin {
                     });
                 }
                 builder.build(b -> recipesByName.put(b.getId(), b.getType().fromJson(b.getId(), b.serializeRecipe())));
+            } else if(r instanceof ProcessingRecipeJS processingRecipeJS) {
+                TieredProcessingRecipeFactory<TieredProcessingRecipe<?>> factory = GreateValues.getFactory(processingRecipeJS.getType());
+                if(factory == null) continue;
+                TieredProcessingRecipeBuilder<TieredProcessingRecipe<?>> builder = new TieredProcessingRecipeBuilder<>(factory, Greate.id("integration/" + processingRecipeJS.id.toString().replace(':', '/')));
+                if(processingRecipeJS.json.has("heatRequirement")) {
+                    builder.requiresHeat(HeatCondition.deserialize(processingRecipeJS.json.get("heatRequirement").getAsString()));
+                }
+                JsonArray ingArr = processingRecipeJS.json.get("ingredients").getAsJsonArray();
+                NonNullList<Ingredient> itemIngredients = NonNullList.create();
+                NonNullList<FluidIngredient> fluidIngredients = NonNullList.create();
+                ingArr.forEach(ing -> {
+                    if(FluidIngredient.isFluidIngredient(ing)) {
+                        fluidIngredients.add(FluidIngredient.deserialize(ing));
+                    } else {
+                        itemIngredients.add(Ingredient.fromJson(ing));
+                    }
+                });
+                if(!itemIngredients.isEmpty()) builder.withItemIngredients(itemIngredients);
+                if(!fluidIngredients.isEmpty()) builder.withFluidIngredients(fluidIngredients);
+
+                JsonArray resultArr = processingRecipeJS.json.get("results").getAsJsonArray();
+                NonNullList<ProcessingOutput> itemResults = NonNullList.create();
+                NonNullList<FluidStack> fluidResults = NonNullList.create();
+                resultArr.forEach(result -> {
+                    if(FluidIngredient.isFluidIngredient(result)) {
+                        fluidResults.add(FluidIngredient.deserialize(result).getMatchingFluidStacks().get(0));
+                    } else {
+                        int count = 1;
+                        if(result.getAsJsonObject().has("count")) {
+                            count = result.getAsJsonObject().get("count").getAsInt();
+                        }
+                        float chance = 1.0f;
+                        if(result.getAsJsonObject().has("chance")) {
+                            chance = result.getAsJsonObject().get("chance").getAsFloat();
+                        }
+                        itemResults.add(new ProcessingOutput(Ingredient.fromJson(result).getItems()[0].copyWithCount(count), chance));
+                    }
+                });
+                if(!itemResults.isEmpty()) builder.withItemOutputs(itemResults);
+                if(!fluidResults.isEmpty()) builder.withFluidOutputs(fluidResults);
+                builder.build( b -> recipesByName.put(b.getId(), b.getType().fromJson(b.getId(), b.serializeRecipe())));
             }
         }
     }
