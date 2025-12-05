718bb05e99aaf29d2aa858725aef21e99ad87a47
Electrolyte220 <cjm03c@gmail.com>
2025-12-05 00:12:11 -0500

    Fix regular saws not performing recipes

--- src/main/java/electrolyte/greate/mixin/MixinSawBlockEntity.java
+++ src/main/java/electrolyte/greate/mixin/MixinSawBlockEntity.java
@@ -3,16 +3,19 @@ package electrolyte.greate.mixin;
 import com.simibubi.create.content.kinetics.saw.SawBlockEntity;
 import electrolyte.greate.content.kinetics.saw.TieredSawBlockEntity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(SawBlockEntity.class)
 public abstract class MixinSawBlockEntity {
 
+    @Shadow(remap = false) private void applyRecipe() { throw new IllegalStateException("Mixin did not apply!"); }
+
     @Redirect(method = "tick", at = @At(value = "INVOKE", target = "Lcom/simibubi/create/content/kinetics/saw/SawBlockEntity;applyRecipe()V"), remap = false)
     private void greate_tick(SawBlockEntity be) {
         if(be instanceof TieredSawBlockEntity tsbe) {
             tsbe.applyValidRecipe();
-        }
+        } else applyRecipe();
     }
 }
